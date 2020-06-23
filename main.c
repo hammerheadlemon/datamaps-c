505faeb8f5c1521576f1ac8c925835c0289d6c85
Matthew Lemon <lemon@matthewlemon.com>
2020-06-23 17:11:26 +0100

    nicer implementation that I almost understand

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <error.h>
 #include <stdlib.h>
+#include <string.h>
 #include <argp.h>
 
 //https://www.gnu.org/software/libc/manual/html_node/Argp-Example-4.html#Argp-Example-4
@@ -12,7 +13,7 @@ const char *argp_program_bug_address = "datamaps@twentyfoursoftware.com";
 static char doc[] = "datamaps -- extract data from spreadsheets using key values stored in CSV files! That is it.";
 
 // A description of the arguments we accept
-static char args_doc[] = "import export";
+static char args_doc[] = "import|export";
 
 // Keys for options without short options
 #define OPT_ABORT 1  // --abort
@@ -23,7 +24,7 @@ static struct argp_option options[] = {
     {"quiet", 'q', 0, 0, "Don't produce any output"},
     {"silent", 's', 0, OPTION_ALIAS},
     {"output", 'o', "FILE", 0, "Output to FILE instead of standard output"},
-    {"datamap", 'd', 0, 0, "Path to datamap file"},
+    {"datamap", 'd', "PATH", 0, "PATH to datamap file to use instead of default"},
     { 0,0,0,0, "The following options should be grouped together:" },
     {"repeat", 'r', "COUNT", OPTION_ARG_OPTIONAL, "Repeat the output COUNT (default 10) times"},
     {"abort", OPT_ABORT, 0, 0, "Abort before showing any output"},
@@ -33,7 +34,7 @@ static struct argp_option options[] = {
 // Used by main to communicate with parse_opt.
 struct arguments
 {
-    char *arg1;
+    char *operation;
     char **strings;
     int silent, verbose;
     char *output_file;
@@ -73,7 +74,7 @@ static error_t parse_opt (int key, char *arg, struct argp_state *state)
         case ARGP_KEY_ARG:
             // Here we know that state->arg_num == 0, since we force
             // argument parsing to end before any more arguments can get here.
-            arguments->arg1 = arg;
+            arguments->operation = arg;
 
             // Now we consume all the rest of the arguments.
             // state->next is the index in state->argv of the next
@@ -105,21 +106,31 @@ int main(int argc, char *argv[])
     arguments.output_file = "-";
     arguments.repeat = 1;
     arguments.abort = 0;
+    arguments.datamap_path = "";
 
     // Parse our arguments; every option seen by parse_opt will be
     // reflected in arguments.
     argp_parse(&argp, argc, argv, 0, 0, &arguments);
     
+    // This should not be called here but it offers a nice message
+    /* argp_help(&argp, stderr, ARGP_HELP_SEE, "datamaps"); */
+    
     if(arguments.abort)
         error(10, 0, "ABORTED");
 
+    if (strcmp("import", arguments.operation) == 0)
+        printf("We are going to call an import() func here.\n");
+    else if (strcmp("export", arguments.operation) == 0)
+        printf("We are going to call an export() func here.\n");
+
     for (i = 0; i < arguments.repeat; ++i) {
-        printf("ARG1 = %s\n", arguments.arg1);
+        printf("ARG1 = %s\n", arguments.operation);
         printf("STRINGS = ");
         for (j = 0; arguments.strings[j]; ++j) {
            printf(j==0 ? "%s" : ", %s", arguments.strings[j]); 
         }
         printf("\n");
+        printf("DATAMAP_PATH = %s\n", arguments.datamap_path);
         printf("OUTPUT_FILE = %s\nVERBOSE = %s\nSILENT = %s\n",
                 arguments.output_file,
                 arguments.verbose ? "yes" : "no",
